76c82b6281bea032bd2a665232e96b06925dff33
Mayank.Gupta <mayank.gupta@iongroup.com>
2019-07-18 15:56:40 +0530

    Changes

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function HomeComponent() {
+    return (
+        <div>
+            <b>This is the first Server Side Rendering Component</b>
+        </div>
+    )
+}
